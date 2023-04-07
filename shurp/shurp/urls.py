dac44e8cd7f7c07d696f29b3e3b101f85f15bfb3
Alvin Haruna <harunaalvin69@gmail.com>
2023-04-07 14:17:51 +0100

    added contact url to path

--- shurp/shurp/urls.py
+++ shurp/shurp/urls.py
@@ -17,12 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index
-
-from core.views import contact
+from core.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
-    path('', contact, name='contact'),
+    path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ]

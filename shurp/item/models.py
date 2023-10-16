c4195220b1476594d3985939e6bb3f272fe22a8e
Alvin Haruna <harunaalvin69@gmail.com>
2023-10-16 09:48:05 +0100

    created items model

--- shurp/item/models.py
+++ shurp/item/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,3 +12,12 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+
+# Create items model
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)

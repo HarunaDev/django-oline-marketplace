93d53beafd3711b5462b1124b4edf1268bb262fe
Alvin Haruna <harunaalvin69@gmail.com>
2023-10-26 08:20:52 +0100

    created models for database
    created fields for category and items, the items class takes in more details regarding an item uploaded on app

--- shurp/item/models.py
+++ shurp/item/models.py
@@ -1,8 +1,10 @@
+#import models
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 
+## create category class that takes name input
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
@@ -10,14 +12,21 @@ class Category(models.Model):
         ordering = ('name',) #bug was a typo 🤣 
         verbose_name_plural = 'Categories'
     
+    # displays the name of the category on dashboard
     def __str__(self):
         return self.name
 
-# Create items model
+# Create items model to get details of an item that should be stored in a database
 class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True,  null=True)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    # displays the name of the item on the dashboard
+    def __str__(self):
+        return self.name

548d47268b5b810082d6155eb5cdc9025c99a6ea
Alvin Haruna <harunaalvin69@gmail.com>
2023-10-27 13:27:14 +0100

    updated views to request first 6 items in data base

--- shurp/core/views.py
+++ shurp/core/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+# import models
+from item.models import Category, Item
+
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    #display first 6 items in db that has not been sold
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+
+    # request category & items
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items,
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')

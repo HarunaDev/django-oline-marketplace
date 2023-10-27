5c6620081da175e826cc3ad0514c2288db58a020
Alvin Haruna <harunaalvin69@gmail.com>
2023-10-27 13:30:15 +0100

    updated urls file to handle static image files

--- shurp/shurp/urls.py
+++ shurp/shurp/urls.py
@@ -14,6 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# handle image *strictly for development
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 
@@ -26,4 +30,4 @@ urlpatterns = [
     path('privacy/', privacy, name='privacy'),
     path('terms/', terms, name='terms'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

70c0662617e9e070d780d01cef1337ac01e10e0b
Alvin Haruna <harunaalvin69@gmail.com>
2024-01-18 08:35:51 +0100

    create view and set url pattern for login page

--- shurp/core/urls.py
+++ shurp/core/urls.py
@@ -9,5 +9,6 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name="index"),
     path('contact/', views.contact, name='contact'),
-    path('signup/', views.signup, name="signup")
+    path('signup/', views.signup, name="signup"),
+    path('login/', views.login, name="login")
 ]
--- shurp/core/views.py
+++ shurp/core/views.py
@@ -45,3 +45,6 @@ def signup(request):
     return render(request, 'core/signup.html', {
         'form': form
     })
+
+def login(request):
+    return render(request, 'core/login.html')

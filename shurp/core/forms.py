641060fd8e853b6c79ed2d1f1827939a0578e616
Alvin Haruna <harunaalvin69@gmail.com>
2024-01-19 12:51:40 +0100

    create login form class

--- shurp/core/forms.py
+++ shurp/core/forms.py
@@ -1,7 +1,19 @@
 #using django to handle creating users and login validation
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+# create login form class
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
 
 # create models for user and and fields they should fill in the form
 class SignupForm(UserCreationForm):

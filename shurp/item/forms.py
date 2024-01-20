bbc4c3e846f2dfd1cac9888091f1fbec4ccf7e01
Alvin Haruna <harunaalvin69@gmail.com>
2024-01-20 10:16:03 +0100

    created form and view for adding items

---
+++ shurp/item/forms.py
@@ -0,0 +1,9 @@
+# create form to handle adding items
+from django import forms
+
+from .models import Item
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category', 'name', 'description', 'price', 'image')

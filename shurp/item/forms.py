86050152abbb5ecf3772c20e1e6072bb21b93a48
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-24 11:07:07 +0100

    added style for new item fields

--- shurp/item/forms.py
+++ shurp/item/forms.py
@@ -3,10 +3,30 @@ from django import forms
 
 from .models import Item
 
+INPUT_CLASSES = ""
+
 # form to allow users add items
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ('category', 'name', 'description', 'price', 'image')
 
+        # style for new item fields
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+        }
         

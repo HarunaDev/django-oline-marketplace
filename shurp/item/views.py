6da60e70a278f662c395769af3a0370b3f719dac
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-25 18:12:34 +0100

    fixed bug that was preventing item from being deleted

--- shurp/item/views.py
+++ shurp/item/views.py
@@ -44,4 +44,4 @@ def delete(request, pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
     item.delete()
 
-    redirect('dashboard/index.html')
+    return redirect('dashboard:index.html')
